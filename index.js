82f45c84fe6a71b4d84eb3687746becc65327374
Akash kumar sahu <akashkumar3002143@gmail.com>
2025-01-02 12:34:19 +0530

    'updating-frontend-url'

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000
 const corsOption = {
     origin: [
         "http://localhost:5173",
+        "https://jobfinder-frontend-prod.vercel.app/",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
