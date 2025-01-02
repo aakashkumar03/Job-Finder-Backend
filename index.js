72212feb580341c8ea16004898657218eb60ea63
Akash kumar sahu <akashkumar3002143@gmail.com>
2025-01-02 12:04:08 +0530

    vercel changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 require("dotenv").config()
-const connectDb = require("./config/database")
-const userRouter = require("./routes/userRouter")
-const jobRouter = require("./routes/jobRouter")
+const connectDb = require("./src/config/database")
+const userRouter = require("./src/routes/userRouter")
+const jobRouter = require("./src/routes/jobRouter")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT || 5000
 
 const corsOption = {
-    origin: '*',
+    origin: [
+        "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }
@@ -19,6 +21,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/",userRouter)
 app.use("/api/", jobRouter)
+app.get("/healthCheck", (req, res) => {
+    res.send("Server is running fine..!!")
+})
 
 connectDb()
 .then(() => {
